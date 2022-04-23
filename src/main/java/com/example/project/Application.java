128011affa571b52be4378e2ea6644f9a76a9559
Nidhi Agrawal <nidhiagr1327@gmail.com>
2022-04-23 23:41:19 +0530

    // added beanNames

--- src/main/java/com/example/project/Application.java
+++ src/main/java/com/example/project/Application.java
@@ -2,11 +2,21 @@ package com.example.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args){
-        SpringApplication.run(Application.class,args);
+
+        ApplicationContext ctx = SpringApplication.run(Application.class,args);
+        System.out.println("Inspecting bean names provided by spring: ");
+        String[] beanNames=ctx.getBeanDefinitionNames(); //for getting bean names
+        Arrays.sort(beanNames);
+        for(String beanName:beanNames){
+            System.out.println(beanName);
+        }
     }
 }
